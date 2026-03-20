26284a76041c923d218a607a71d1fec89127c70d
maxsteeel <109047395+maxsteeel@users.noreply.github.com>
2026-03-20 19:56:35 +0000

    susfs: fix compilation errors

--- kernel/sys.c
+++ kernel/sys.c
@@ -617,6 +617,9 @@ SYSCALL_DEFINE1(setuid, uid_t, uid)
 	return __sys_setuid(uid);
 }
 
+#ifdef CONFIG_KSU_SUSFS
+extern int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid);
+#endif
 
 /*
  * This function implements a generic ability to update ruid, euid,
