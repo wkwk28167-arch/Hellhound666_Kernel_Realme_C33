247b54fabb130e72c071d0d1a3dba9e3d62c2794
maxsteeel <109047395+maxsteeel@users.noreply.github.com>
2026-03-28 18:38:08 +0000

    make susfs inline hooks usable as manual hooks
    I know this is ugly, and even worse than manual hooks, but I'm just lazy, so this is a workaround to reuse susfs inline hooks as if they were manual hooks, maintaining compatibility with susfs

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -301,6 +301,10 @@ EXPORT_SYMBOL_GPL(kernel_power_off);
 
 DEFINE_MUTEX(system_transition_mutex);
 
+#if defined(CONFIG_KSU_SUSFS) || defined(CONFIG_KSU_MANUAL_HOOK)
+extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
+#endif
+
 /*
  * Reboot system call: for obvious reasons only root may call it,
  * and even root needs to set up some magic numbers in the registers
@@ -311,9 +315,6 @@ DEFINE_MUTEX(system_transition_mutex);
 
  */
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
-extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
-#endif
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
@@ -321,8 +322,13 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	char buffer[256];
 	int ret = 0;
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+#if defined(CONFIG_KSU_SUSFS) || defined(CONFIG_KSU_MANUAL_HOOK)
+	ret = ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+	if (ret) {
+		goto orig_flow;
+	}
+	return ret;
+orig_flow:
 #endif
 
 	/* We only trust the superuser with rebooting the system. */
